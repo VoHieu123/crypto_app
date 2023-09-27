1e5a47cbeabe819016d9c1b1b4600678c2a03cef
hieuvtt1 <hieu.vtt186104@gmail.com>
2023-09-27 16:41:53 +0700

    Remove shit

--- const.py
+++ const.py
@@ -3,10 +3,3 @@ BIN_SECRET_KEY = "4mwZ8eVpMBfOn2nw69BRqsdNfdCMauPJnUvCfIEZwIKKe89cAxcO48CR31pdic
 OKX_API_KEY = "806b691e-dee6-4ddc-833c-181b449299ce"
 OKX_SECRET_KEY = "12B17E4D0434B6EE348DF86CE0FF8A05"
 OKX_PASSPHRASE = "Q5tpnwErKa1J(gfsIHY96"
-# Todo: 1 output excel file
-COIN_PRICE_FILE = "coin_prices.xlsx"
-BID_ASK_FILE = "bid_ask_data.xlsx"
-FUNDING_RATE_FILE = "funding_rate_data.xlsx"
-FUNDING_RATE_USDT_FILE = "funding_rate_usdt_data.xlsx"
-FUNDING_RATE_PKL = "funding_rate_data.pkl"
-NOTIFY_RATE_S = 600
