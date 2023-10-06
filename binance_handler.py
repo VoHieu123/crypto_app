ed9ba13581dd58a7c89787c90b1d7da163b3313e
hieuvtt1 <hieu.vtt186104@gmail.com>
2023-10-06 17:54:44 +0700

    Save

--- binance_handler.py
+++ binance_handler.py
@@ -18,20 +18,26 @@ class BinanceHandler:
                 continue
             risk_list[f"BiMC_{asset['asset']}"] = float(asset["maintMargin"])/float(asset["marginBalance"])
 
-        sub_accounts = ["evan1965.11@proton.me", "evan1965.12@proton.me", "evan1965.13@proton.me"]
-        for i, sub_account in enumerate(sub_accounts):
-            usdm = self.binanceClient.get_subaccount_futures_details(email=sub_account, futuresType = 1)
-            usdm = usdm["futureAccountResp"]["assets"]
-            for usd in usdm:
-                if int(float(usd["walletBalance"])) == 0 or int(float(usd["maintenanceMargin"])) == 0:
-                    continue
-                risk_list[f"Bi{i + 1}U_{usd['asset']}"] = float(usd["maintenanceMargin"])/float(usd["marginBalance"])
+        try:
+            sub_accounts_raw = self.binanceClient.get_sub_account_list()
+            sub_accounts = []
+            for sub_account in sub_accounts_raw["subAccounts"]:
+                sub_accounts.append(sub_account["email"])
+            for i, sub_account in enumerate(sub_accounts):
+                usdm = self.binanceClient.get_subaccount_futures_details(email=sub_account, futuresType = 1)
+                usdm = usdm["futureAccountResp"]["assets"]
+                for usd in usdm:
+                    if int(float(usd["walletBalance"])) == 0 or int(float(usd["maintenanceMargin"])) == 0:
+                        continue
+                    risk_list[f"Bi{i + 1}U_{usd['asset']}"] = float(usd["maintenanceMargin"])/float(usd["marginBalance"])
 
-            coinm = self.binanceClient.get_subaccount_futures_details(email=sub_account, futuresType = 2)
-            coinm = coinm["deliveryAccountResp"]["assets"]
-            for coin in coinm:
-                if int(float(coin["walletBalance"])) == 0 or int(float(coin["maintenanceMargin"])) == 0:
-                    continue
-                risk_list[f"Bi{i + 1}C_{coin['asset']}"] = float(coin["maintenanceMargin"])/float(coin["marginBalance"])
+                coinm = self.binanceClient.get_subaccount_futures_details(email=sub_account, futuresType = 2)
+                coinm = coinm["deliveryAccountResp"]["assets"]
+                for coin in coinm:
+                    if int(float(coin["walletBalance"])) == 0 or int(float(coin["maintenanceMargin"])) == 0:
+                        continue
+                    risk_list[f"Bi{i + 1}C_{coin['asset']}"] = float(coin["maintenanceMargin"])/float(coin["marginBalance"])
+        except:
+            pass
 
         return risk_list
