7c16dac516e130da84d75131d87c8dc5fd9b9d90
hieuvtt1 <hieu.vtt186104@gmail.com>
2023-10-14 10:12:59 +0700

    Save

--- main.py
+++ main.py
@@ -23,8 +23,7 @@ def backgroundTask(controller):
         controller.loop()
 
 def main():
-    # choice = int(input("Type: 1 - Tuan Anh, 2 - Steve: "))
-    choice = 1
+    choice = int(input("Type: 1 - Tuan Anh, 2 - Steve: "))
 
     while True:
         if choice == 1:
