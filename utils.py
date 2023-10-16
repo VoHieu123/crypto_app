f9834374c7a6204372d430f1a14c87af709aa0e5
hieuvtt1 <hieu.vtt186104@gmail.com>
2023-10-16 09:34:51 +0700

    S

--- utils.py
+++ utils.py
@@ -1,9 +1,4 @@
 import uuid
-from decimal import Decimal, ROUND_HALF_UP
-
-def round_to_auto_decimal_places(number):
-    decimal_number = Decimal(str(number))
-    return decimal_number.quantize(Decimal('1.000'))
 
 def convert_to_float(data):
     if isinstance(data, dict):
@@ -22,17 +17,18 @@ def generate_uuid():
     return str(uuid.uuid4())
 
 def auto_format(number):
-    num_str = str(number)
+    return str(round(number, 6))
+    # num_str = str(number)
 
-    if '.' in num_str:
-        integer_part, decimal_part = num_str.split('.')
-        integer_len = len(integer_part)
-    else:
-        integer_len = 0
+    # if '.' in num_str:
+    #     integer_part, decimal_part = num_str.split('.')
+    #     integer_len = len(integer_part)
+    # else:
+    #     integer_len = 0
 
-    if (abs(number) > 0.0001):
-        returnStr = str(round(number, 5 - integer_len if integer_len < 3 else None))
-    else:
-        returnStr = str(round(number, 5))
+    # if (abs(number) > 0.0001):
+    #     returnStr = str(round(number, 5 - integer_len if integer_len < 3 else None))
+    # else:
+    #     returnStr = str(round(number, 5))
 
-    return returnStr
+    # return returnStr
