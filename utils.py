3035ecb96d997f208d8e7781eaeb41d063ee4e12
hieuvtt1 <hieu.vtt186104@gmail.com>
2023-10-31 07:22:01 +0700

    S

--- utils.py
+++ utils.py
@@ -1,13 +1,39 @@
 import uuid
 from PyQt6.QtCore import pyqtSignal, QObject
-import subprocess
+import socket, struct, win32api, datetime
 
 def resynch():
-    try:
-        subprocess.run(['w32tm', '/resync'], check=True)
-    except:
-        # print("Synchronize failed!")
-        pass
+    # List of servers in order of attempt of fetching
+    server_list = ['ntp.iitb.ac.in', 'time.nist.gov', 'time.windows.com', 'pool.ntp.org']
+
+    def gettime_ntp(addr='time.nist.gov'):
+        TIME1970 = 2208988800
+        client = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+        data = '\x1b' + 47 * '\0'
+        try:
+            # Timing out the connection after 5 seconds, if no response received
+            client.settimeout(5.0)
+            client.sendto( data, (addr, 123))
+            data, address = client.recvfrom( 1024 )
+            if data:
+                epoch_time = struct.unpack( '!12I', data )[10]
+                epoch_time -= TIME1970
+                return epoch_time
+        except socket.timeout:
+            return None
+
+    for server in server_list:
+        epoch_time = gettime_ntp(server)
+        if epoch_time is not None:
+            # SetSystemTime takes time as argument in UTC time. UTC time is obtained using utcfromtimestamp()
+            utcTime = datetime.datetime.utcfromtimestamp(epoch_time)
+            win32api.SetSystemTime(utcTime.year, utcTime.month, utcTime.weekday(), utcTime.day, utcTime.hour, utcTime.minute, utcTime.second, 0)
+            # Local time is obtained using fromtimestamp()
+            localTime = datetime.datetime.fromtimestamp(epoch_time)
+            print("Time updated to: " + localTime.strftime("%Y-%m-%d %H:%M") + " from " + server)
+            break
+        else:
+            print("Could not find time from " + server)
 
 class Communication(QObject):
     ui_signal = pyqtSignal()
