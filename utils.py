608faeec9960b51d3b17c4904bfd57677dd646ea
hieuvtt1 <hieu.vtt186104@gmail.com>
2023-10-31 08:45:16 +0700

    S

--- utils.py
+++ utils.py
@@ -1,39 +1,50 @@
 import uuid
 from PyQt6.QtCore import pyqtSignal, QObject
-import socket, struct, win32api, datetime
+import win32api, datetime, time
+from binance import Client
+from pybit.unified_trading import HTTP
+from okx import PublicData
+
+okx_client = PublicData.PublicAPI(debug=False)
+bybit_client = HTTP(testnet=False)
+binance_client = Client()
+clients = {"Binance": binance_client, "Bybit": bybit_client, "OKX": okx_client}
 
 def resynch():
-    # List of servers in order of attempt of fetching
-    server_list = ['ntp.iitb.ac.in', 'time.nist.gov', 'time.windows.com', 'pool.ntp.org']
-
-    def gettime_ntp(addr='time.nist.gov'):
-        TIME1970 = 2208988800
-        client = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
-        data = '\x1b' + 47 * '\0'
+    def get_time(client):
         try:
-            # Timing out the connection after 5 seconds, if no response received
-            client.settimeout(5.0)
-            client.sendto( data, (addr, 123))
-            data, address = client.recvfrom( 1024 )
-            if data:
-                epoch_time = struct.unpack( '!12I', data )[10]
-                epoch_time -= TIME1970
-                return epoch_time
-        except socket.timeout:
+            if client == binance_client:
+                print("Getting time from Binance")
+                time = int(client.get_server_time()["serverTime"])
+                print(f"Binance time: {time}")
+            elif client == bybit_client:
+                print("Getting time from Bybit")
+                time = int(client.get_server_time()["time"])
+                print(f"Bybit time: {time}")
+            elif client == okx_client:
+                print("Getting time from OKX")
+                time = int(client.get_system_time()["data"][0]["ts"])
+                print(f"OKX time: {time}")
+            print(f"Local time: {time.time()}")
+            return time // 1000
+        except Exception as e:
+            print(e)
             return None
 
-    for server in server_list:
-        epoch_time = gettime_ntp(server)
+    for client_name, client in clients.items():
+        epoch_time = get_time(client)
         if epoch_time is not None:
-            # SetSystemTime takes time as argument in UTC time. UTC time is obtained using utcfromtimestamp()
             utcTime = datetime.datetime.utcfromtimestamp(epoch_time)
-            win32api.SetSystemTime(utcTime.year, utcTime.month, utcTime.weekday(), utcTime.day, utcTime.hour, utcTime.minute, utcTime.second, 0)
-            # Local time is obtained using fromtimestamp()
+            try:
+                win32api.SetSystemTime(utcTime.year, utcTime.month, 0, utcTime.day, utcTime.hour, utcTime.minute, utcTime.second, 0)
+            except Exception as e:
+                print(e)
+                break
             localTime = datetime.datetime.fromtimestamp(epoch_time)
-            print("Time updated to: " + localTime.strftime("%Y-%m-%d %H:%M") + " from " + server)
-            break
-        else:
-            print("Could not find time from " + server)
+            print("Time updated to: " + localTime.strftime("%Y-%m-%d %H:%M") + "from" + client_name)
+            return
+
+    print("Could not update time")
 
 class Communication(QObject):
     ui_signal = pyqtSignal()
